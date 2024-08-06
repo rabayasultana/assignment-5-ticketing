5530aaaacb564273f9b9df43f3dd699900db26b6
RabayaSultana <rabayashova811@gmail.com>
2024-08-06 21:37:44 +0600

    Scrolldown button done

--- js/ticketing.js
+++ js/ticketing.js
@@ -8,3 +8,8 @@ tailwind.config = {
       },
     },
   };
+
+  document.getElementById('scroll-down-btn').addEventListener('click',function(){
+const targetSection = document.getElementById('target-section');
+targetSection.scrollIntoView({ behavior: 'smooth' })
+  })
